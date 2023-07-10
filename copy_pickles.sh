1501148e7a6bd6b744585acdccabdb7e902c035a
soumensardar <soumen.sardar@smithsdetection.com>
2023-07-10 16:42:17 +0530

    - script to copy files to stackgan

--- copy_pickles.sh
+++ copy_pickles.sh
@@ -1,4 +1,5 @@
-#sh copy_pickle.sh "output/jointemb995_word_predict_2023_07_10_16_32_43" "../StackGAN-Pytorch/data/sixray_2381"
+# ####################################
+# sh copy_pickle.sh "output/jointemb995_word_predict_2023_07_10_16_32_43" "../StackGAN-Pytorch/data/sixray_2381"
 cp "output/$1/Image/filenames_bulk_jemb.pickle" "$2/train/filenames.pickle"
 cp "output/$1/Image/embedding_bulk_word_1536_jemb.pickle" "$2/train/embedding_bulk_word_1536_jemb.pickle"
 cp "output/$1/Image/embedding_test_bulk_word_1536_jemb.pickle" "$2/train/embedding_bulk_word_1536_jemb.pickle"

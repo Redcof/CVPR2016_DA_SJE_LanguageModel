1f6449e6821e0aba0b04da13c62b7cb2d6b2ea72
soumensardar <soumen.sardar@smithsdetection.com>
2023-07-10 16:25:21 +0530

    - production with checkpoint 995

--- predict_script_word.sh
+++ predict_script_word.sh
@@ -1,9 +1,9 @@
 sh cuda_mem.sh
-python main.py --name "jointemb955" \
+python main.py --name "jointemb995" \
 --data-dir "../StackGAN-Pytorch/data/sixray_2381/train/captions" \
 --class-name-file "../StackGAN-Pytorch/data/sixray_2381/train/classes.txt" \
 --label-csv "../StackGAN-Pytorch/data/sixray_2381/train/labels.csv" \
---NET-TXT "output/pydsje_word_train_2023_07_08_11_32_47/Model/netTXT_epoch_955.pth" \
+--NET-TXT "output/pydsje_word_train_2023_07_08_11_32_47/Model/netTXT_epoch_995.pth" \
 --vocabulary-txt "output/pydsje_word_train_2023_07_08_11_32_47/Log/vocabulary.txt" \
 --drop-last \
 --emd-dim 1536 \
@@ -11,7 +11,6 @@ python main.py --name "jointemb955" \
 --doc-length 70 \
 --embedding-strategy word \
 --predict \
---bulk 4 \
---cuda
+--bulk 4
 
 
